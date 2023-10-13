fec3c69e492aad9ded0d98d4f283c95c3357fa1c
EmirOZCELIK <42338841+animyrch@users.noreply.github.com>
2023-10-13 22:07:45 +0200

    improvement for video recording
    we can now see ourselves while recording

--- src/lib/components/Recorder.svelte
+++ src/lib/components/Recorder.svelte
@@ -1,48 +1,64 @@
 <script>
-    let mediaRecorder;
-    let recordedChunks = [];
-  
-    async function startRecording() {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-        mediaRecorder = new MediaRecorder(stream);
-  
-        mediaRecorder.ondataavailable = (event) => {
-          if (event.data.size > 0) {
-            recordedChunks.push(event.data);
-          }
-        };
-  
-        mediaRecorder.onstop = () => {
-          const blob = new Blob(recordedChunks, { type: 'video/webm' });
-          const url = URL.createObjectURL(blob);
-          const a = document.createElement('a');
-          a.href = url;
-          a.download = 'recorded-video.webm';
-          a.click();
-          URL.revokeObjectURL(url);
-          recordedChunks = [];
-        };
-  
-        mediaRecorder.start();
-      } catch (error) {
-        console.error('Error accessing camera:', error);
-      }
+  let mediaStream;
+  let mediaRecorder;
+  let recordedChunks = [];
+  let videoElement;
+
+  const startRecording = async () => {
+    try {
+      mediaStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+      mediaRecorder = new MediaRecorder(mediaStream);
+
+      mediaRecorder.ondataavailable = (event) => {
+        if (event.data.size > 0) {
+          recordedChunks.push(event.data);
+        }
+      };
+
+      mediaRecorder.onstop = () => {
+        const recordedBlob = new Blob(recordedChunks, { type: 'video/webm' });
+        const recordedUrl = URL.createObjectURL(recordedBlob);
+
+        // Set the recorded video as the source of the video element
+        videoElement.src = recordedUrl;
+
+        // Create a download link
+        const downloadLink = document.createElement('a');
+        downloadLink.href = recordedUrl;
+        downloadLink.download = 'recorded-video.webm';
+        downloadLink.textContent = 'Download Video';
+
+        // Append the download link to the document
+        document.body.appendChild(downloadLink);
+
+        // Unmute the audio
+        videoElement.muted = false;
+      };
+
+      // Set the live video feed as the source of the video element
+      videoElement.srcObject = mediaStream;
+
+      // Mute the audio during recording
+      videoElement.muted = true;
+
+      // Start the recording
+      mediaRecorder.start();
+    } catch (error) {
+      console.error('Error accessing media devices:', error);
     }
-  
-    function stopRecording() {
-      if (mediaRecorder) {
-        mediaRecorder.stop();
-      }
+  };
+
+  const stopRecording = () => {
+    if (mediaRecorder && mediaRecorder.state !== 'inactive') {
+      mediaRecorder.stop();
+      mediaStream.getTracks().forEach((track) => track.stop());
     }
-  </script>
-  
-  <style>
-    /* Add your styles here */
-  </style>
-  
-  <div>
-    <button on:click={startRecording}>Start Recording</button>
-    <button on:click={stopRecording}>Stop Recording</button>
-  </div>
-  
+  };
+</script>
+
+<div>
+  <button on:click={startRecording}>Start Recording</button>
+  <button on:click={stopRecording}>Stop Recording</button>
+</div>
+
+<video bind:this={videoElement} controls autoplay />
