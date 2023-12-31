6e25ec7b8568b7a83c10d9bdcbb2b4172fd09d42
EmirOZCELIK <42338841+animyrch@users.noreply.github.com>
2023-12-31 15:36:01 +0100

    refactor

--- src/lib/helpers/routing.js
+++ src/lib/helpers/routing.js
@@ -3,17 +3,8 @@ import { browser } from '$app/environment';
 import { showToast } from '$lib/stores/toast';
 
 export function handlePrivateRoute() {
-    // redirectURL.setRedirectURL(location.href)
     if (browser) {
         goto('/');
     }
-
     showToast('You need to login to access this page!');
-    // Swal.fire({
-    // title: 'You are not authenticated',
-    // text: 'Please log in or sign up to view this page',
-    // type: 'error',
-    // allowOutsideClick: false,
-    // confirmButtonText: 'Will do!',
-    // })
 }
