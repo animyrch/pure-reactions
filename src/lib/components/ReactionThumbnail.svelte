d9e6b1e4f2391a187e4affcca16c9d394f13d031
EmirOZCELIK <42338841+animyrch@users.noreply.github.com>
2024-06-10 00:46:39 +0200

    Modifed reaction thumbnail links to simplify reaction listing interface

--- src/lib/components/ReactionThumbnail.svelte
+++ src/lib/components/ReactionThumbnail.svelte
@@ -1,5 +1,7 @@
 <script>
     import VideoAuthor from '$lib/components/VideoAuthor.svelte';
+    import { DotsVerticalOutline } from 'flowbite-svelte-icons';
+    import { Popover, Button } from 'flowbite-svelte';
 
     export let reactionPageId;
     export let reactionVideoId;
@@ -19,16 +21,44 @@
         </div>
     </div>
 </a>
-<div>
-    <div class="flex gap-2">
-        <p>{reactionVideoTitle || originalVideoTitle}</p>
+<div class="flex items-center">
+    <div
+        class="flex-1 truncate"
+    >
+        <div
+        >
+            <p
+                class="truncate"
+                title="{reactionVideoTitle || originalVideoTitle}"
+            >{reactionVideoTitle || originalVideoTitle}</p>
+        </div>
+        {#if reactionVideoAuthor}
+            <VideoAuthor
+                videoAuthor={reactionVideoAuthor}
+                showLinks={false}
+                isReactor
+            />
+        {/if}
     </div>
-    {#if reactionVideoAuthor}
-        <VideoAuthor
-            videoAuthor={reactionVideoAuthor}
-            isReactor
-        />
-    {/if}
+    <div
+        class="flex-shrink-0"
+    >
+        <button
+            id="{`offset-${reactionPageId}`}"
+        >
+            <DotsVerticalOutline size="md"/>
+        </button>
+        <Popover class="w-42 text-sm font-light z-100" placement="left" triggeredBy="{`#offset-${reactionPageId}`}" trigger="click">
+            <ul>
+                <li class="flex items-center mb-1 z-100">
+                    <a target="_blank" href={`https://www.youtube.com/${reactionVideoAuthor}`}>Open Youtube Page</a>
+                </li>
+                <li class="flex items-center mb-1">
+                    <a href={`reactor/${reactionVideoAuthor}`}>Open Reactor Page</a>
+                </li>
+            </ul>
+        </Popover>
+    </div>
 </div>
 <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
 
--- src/lib/helpers/youtube.js
+++ src/lib/helpers/youtube.js
@@ -6,6 +6,7 @@ import {
 
 const getBasicVideoDetailsWithEmbedApi = async (videoId) => {
     try {
+      console.log('Fetching data for video:', basicVideoDetailsUrl.replace(videoIdPlaceholder, videoId));
         const response = await fetch(basicVideoDetailsUrl.replace(videoIdPlaceholder, videoId));
         const data = await response.json();
         return data;
