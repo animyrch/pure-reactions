1c7dbd35f43aa03e40f4fc520b5fa11264100278
EmirOZCELIK <42338841+animyrch@users.noreply.github.com>
2024-06-16 16:02:24 +0200

    we now handle youtube short video urls as well

--- src/lib/helpers/youtube.js
+++ src/lib/helpers/youtube.js
@@ -32,20 +32,11 @@ export const getAuthorFromAuthorUrl = (authorUrl) => {
  * 'https://youtu.be/LEv2fMoVvXE?si=SNxsWCJfigFYILvg';
  * 'https://www.youtube.com/watch?v=LEv2fMoVvXE&ab_channel=AsmongoldTV';
  * 'https://www.youtube.com/watch?v=LEv2fMoVvXE';
+ * 'https://www.youtube.com/shorts/3JnmAl_8W5k'
  * 'LEv2fMoVvXE';
  */
-export const extractYouTubeVideoId = (urlOrVideoId) => {
-  // Regular expression to match YouTube video IDs in different URL formats
-  const regex = /(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
-  
-  // Check if the input is already a YouTube video ID
-  if (urlOrVideoId.match(/^[a-zA-Z0-9_-]{11}$/)) {
-    return urlOrVideoId; // Return the input as it is
-  }
-
-  // Match the video ID using the regular expression
-  const match = urlOrVideoId.match(regex);
-
-  // Return the video ID if a match is found, otherwise return null
-  return match ? match[1] : null;
+export const extractYouTubeVideoId = (url) => {
+  const regex = /^([a-zA-Z0-9_-]{11})$|(youtu\.be\/|youtube\.com\/(watch\?(.*&)?v=|(embed|v|shorts)\/))([^\?&"'>]+)/;
+  const matches = url.match(regex);
+  return matches ? (matches[1] || matches[6]) : null;
 };
