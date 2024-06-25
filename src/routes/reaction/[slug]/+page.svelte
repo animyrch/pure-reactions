defe391560bb77fb32d544d3be6760186b1b9f46
EmirOZCELIK <42338841+animyrch@users.noreply.github.com>
2024-06-25 20:03:46 +0200

    Revert "First draft of RunTimeEditor"
    This reverts commit 80767a2d5aa899c1cac24dd687393098253aa722.

--- src/routes/reaction/[slug]/+page.svelte
+++ src/routes/reaction/[slug]/+page.svelte
@@ -17,7 +17,6 @@
   import { downloadBasicVideoDetails } from '$lib/helpers/youtube';
   import CreatorDetails from "$lib/components/Video/CreatorDetails.svelte";
   import { ExpandSolid, MinimizeSolid } from 'flowbite-svelte-icons';
-    import RunTimeEditor from "../../../lib/components/Video/RunTimeEditor.svelte";
 
   export let data;
 
@@ -539,7 +538,6 @@
       </div>
     </div>
   {/if}
-  <RunTimeEditor />
 </div>
 
 <style>
